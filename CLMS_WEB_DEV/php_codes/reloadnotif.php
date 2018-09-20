843fae7774dbfe82d460dbf16604bef998b00c42
Akumao <mark_reborn13@yahoo.com>
2018-09-20 17:45:25 +0800

    Update(20.09.2018)

--- CLMS_WEB_DEV/php_codes/reloadnotif.php
+++ CLMS_WEB_DEV/php_codes/reloadnotif.php
@@ -15,7 +15,7 @@
           // GET ADDITIONAL INFO ON RECEIVED NOTIF
           if($NotifType=='Received')
           {
-            $receive_sqltxt="Select [Serial].[SerialName],[ReceiveSerial].[RSDate],[ReceiveSerial].[DepartmentID] from [Serial] inner Join [Notification] ON [Serial].[SerialID]=[Notification].[SerialID] left join [ReceiveSerial] ON [Notification].[SerialID]=[ReceiveSerial].[SerialID] WHERE [ReceiveSerial].[Status]=? AND [NotificationSeen]=? AND [Serial].[SerialName]=?";
+            $receive_sqltxt="Select [Serial].[SerialName],[Notification].[Date_Receive_RedFlag],[ReceiveSerial].[DepartmentID] from [Serial] inner Join [Notification] ON [Serial].[SerialID]=[Notification].[SerialID] left join [ReceiveSerial] ON [Notification].[SerialID]=[ReceiveSerial].[SerialID] WHERE [ReceiveSerial].[Status]=? AND [NotificationSeen]=? AND [Serial].[SerialName]=?";
             $receive_query=sqlsrv_query($conn,$receive_sqltxt,array('Received','NotSeen',$Sname),$opt);
 
             $receive_row=sqlsrv_fetch_array($receive_query,SQLSRV_FETCH_ASSOC);
@@ -23,7 +23,7 @@
             if($receive_row!=null)
             {
               $receive_sname=$receive_row['SerialName'];
-              $receive_RSDATE=$receive_row['RSDate']->format('M-d-Y');
+              $receive_RSDATE=$receive_row['Date_Receive_RedFlag']->format('M-d-Y');
               $receive_Dept=$receive_row['DepartmentID'];
 
               //SETTING VALUE OF COURSENAME
@@ -75,7 +75,7 @@
           // GET ADDITIONAL INFO ON DELEYED NOTIF
           else
           {
-            $delivery_sqltxt="Select [Serial].[SerialName],[Notification].[NotificationType],[Notification].[NotificationSeen],[Distributor].[DistributorName],[Delivery].[ExpectedReceiveDate] from Serial Inner Join [Notification] ON [Serial].[SerialID]=[Notification].[SerialID] Right JOIN Subscription ON [Notification].[SerialID]=[Subscription].[SerialID] INNER JOIN Delivery ON [Subscription].[DistributorID]=[Delivery].[DistributorID] LEFT JOIN [Distributor] ON [Delivery].[DistributorID]=[Distributor].[DistributorID] Where [Notification].[NotificationSeen]=? AND [Notification].[NotificationType]=? AND [Serial].[SerialName]=?";
+            $delivery_sqltxt="Select [Serial].[SerialName],[Notification].[NotificationType],[Notification].[NotificationSeen],[Distributor].[DistributorName],[Delivery].[ExpectedReceiveDate],[Notification].[Date_Receive_RedFlag] from Serial Inner Join [Notification] ON [Serial].[SerialID]=[Notification].[SerialID] Right JOIN Subscription ON [Notification].[SerialID]=[Subscription].[SerialID] INNER JOIN Delivery ON [Subscription].[DistributorID]=[Delivery].[DistributorID] LEFT JOIN [Distributor] ON [Delivery].[DistributorID]=[Distributor].[DistributorID] Where [Notification].[NotificationSeen]=? AND [Notification].[NotificationType]=? AND [Serial].[SerialName]=?";
 
             $delivery_query=sqlsrv_query($conn,$delivery_sqltxt,array('NotSeen','DeleyedDeliver',$Sname),$opt);
            
@@ -85,7 +85,7 @@
               {
                 $delivery_sname=$delivery_row['SerialName'];
                 $delivery_DisbName=$delivery_row['DistributorName'];
-                $delivery_ERD_deleyed=$delivery_row['ExpectedReceiveDate']->modify('+1 day')->format('M-d-Y');
+                $delivery_ERD_deleyed=$delivery_row['Date_Receive_RedFlag']->format('M-d-Y');
 
 
                 echo '
