8c59a97db9781fb9a6af6575b941a67a748345ac
Akumao <mark_reborn13@yahoo.com>
2018-12-04 20:25:07 +0800

    test1

--- CLMS_WEB_DEV/SSP/serverside_currentSubs.php
+++ CLMS_WEB_DEV/SSP/serverside_currentSubs.php
@@ -33,7 +33,7 @@ else
 {
 	$rid=$_POST['rid'];
 	echo json_encode(
-	SSP::complex( $_POST, $sql_details, $table, $primary_key, $columns,null,"Status='OnGoing' AND (Subscription_Date BETWEEN LEFT('".$rid."',4) AND RIGHT('".$rid."',4)) AND DistributorName='".$dept."' AND Remove IS NULL AND arc is NULL" ));
+	SSP::complex( $_POST, $sql_details, $table, $primary_key, $columns,null,"Status='OnGoing' AND ((Subscription_Date BETWEEN LEFT('".$rid."',4) AND RIGHT('".$rid."',4)) Or Status='OnGoing') AND DistributorName='".$dept."' AND Remove IS NULL AND arc is NULL" ));
 }
 
 
