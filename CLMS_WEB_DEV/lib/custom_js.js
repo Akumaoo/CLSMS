dda65d23c23bc9e33cbdff0095956fbf88831839
Akumao <mark_reborn13@yahoo.com>
2018-12-05 16:45:11 +0800

    UPDATE DISABLE NEGATIVE NUMBERS

--- CLMS_WEB_DEV/lib/custom_js.js
+++ CLMS_WEB_DEV/lib/custom_js.js
@@ -1,5 +1,17 @@
 $(function(){
 
+	$('input[type="number"]').keydown(function(e){
+		if(e.keyCode==189 || e.keyCode==69)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	});
+
+	$('input[type="number"]').attr('min', '0');
 
 	$('#remove-active').click(function() {
 		if($('#remove-active').hasClass('active'))
